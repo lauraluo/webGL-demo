21c64d791e67cd4880054411de524aaf59f3a08e
Laura Luo <laura.luo@gmail.com>
2017-07-19 08:34:30 +0800

    lala

--- src/audio.jsx
+++ src/audio.jsx
@@ -1,6 +1,5 @@
 import Tone from 'Tone';
 
-
 var scene;
 var camera;
 var renderer;
@@ -11,7 +10,7 @@ var myCanvas_W;
 var myCanvas_H;
 var controls;
 var background = '#000';
-var surface = '#fffbbf';
+var surface = '#363636';
 worldWidth = window.innerWidth;
 
 var waveform = new Tone.Analyser('waveform', size);
@@ -31,7 +30,7 @@ var gridSizeY = 200;
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 8000);
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 12000);
 
     renderer = new THREE.WebGLRenderer({
         alpha: true
@@ -46,17 +45,16 @@ function init() {
         gridSizeY - 1
     );
     material = new THREE.MeshLambertMaterial({
-        color: '#fff',
         emissive: '#fff',
         wireframe: true,
         vertexColors: THREE.FaceColors,
-        wireframeLinewidth: 2
+        wireframeLinewidth: 1
     });
     geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 
     var inc = 0;
     data = [];
-    var highestPoint = 50;
+    var highestPoint = 0;
 
     for (var x = 0; x < gridSizeX; x++) {
         data.push([]);
@@ -68,27 +66,30 @@ function init() {
         }
     }
 
-    terrain = new THREE.Mesh(geometry, material);
-    terrain.position.z = -580;
-    terrain.position.x = 0;
-    terrain.position.y = 90;
-    terrain.rotation.y = -1.5 * Math.PI;
-    terrain.rotation.x = -0.97 * Math.PI;
-    terrain.rotation.z = 0.03 * Math.PI;
-    scene.add(terrain);
-    scene.fog = new THREE.FogExp2('#000', 0.002);
     
 
-    var directionalLight = new THREE.DirectionalLight(background);
-    directionalLight.position.set(100, 1, -21000).normalize();
-    scene.add(directionalLight);
+    terrain = new THREE.Mesh(geometry, material);
+    terrain.position.z = 0
+    terrain.position.x = 0;
+    terrain.position.y = 0;
+    terrain.rotation.y = -1.5 * Math.PI;
+    terrain.rotation.x = -1 * Math.PI;
+    terrain.rotation.z = 0.07 * Math.PI;
+    scene.add(terrain);
+    scene.fog = new THREE.FogExp2(background, 0.0019);
+
 
     // position and point the camera to the center of the scene
     camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 0;
+    camera.position.y = -100* window.innerWidth / window.innerHeight;
+    camera.position.z = 400 * window.innerWidth / window.innerHeight;
 
-    camera.lookAt(scene.position);
+    
+    
+    var directionalLight = new THREE.DirectionalLight('#fff');
+
+    directionalLight.position.set(400, 60, 0);
+    scene.add(directionalLight);
 
     document.getElementById('audio').appendChild(renderer.domElement);
 
@@ -105,7 +106,7 @@ function updateTerrain(values) {
             } else {
                 data[x][y] = data[x][y - 1];
             }
-            geometry.vertices[inc].y = data[x][y];
+            geometry.vertices[inc].y = data[x][y]*0.8;
             inc++;
         }
     }
@@ -114,7 +115,7 @@ function updateTerrain(values) {
 
 function getval(x, y) {
     var val = 0;
-    val = 1 * 25;
+    val = 1 * 128;
     val += Math.sin(x * 2 / gridSizeX) * 1;
     return val;
 }
@@ -123,13 +124,7 @@ function renderView() {
     var waveValues = waveform.analyse();
 
     updateTerrain(waveValues);
-
-    // controls.update()
-    // camera.position.fromArray(controls.position)
-    // camera.up.fromArray(controls.up)
-
     renderer.render(scene, camera);
-
     requestAnimationFrame(renderView);
 }
 
